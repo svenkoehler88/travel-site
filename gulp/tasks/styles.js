bee3c92fa0b7531e8c8d208746f4fb6fb21e5466
Sven <14svenkoehler28@gmail.com>
2018-05-25 19:57:04 +0200

    Completed the our features secton.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -10,8 +10,8 @@ gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
-    	console.log(errorInfo.toString());
-    	this.emit('end');
+      console.log(errorInfo.toString());
+      this.emit('end');
     })
     .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-browserSync = require('browser-sync').create(); 
+browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
@@ -12,7 +12,7 @@ gulp.task('watch', function() {
   });
 
   watch('./app/index.html', function() {
-   browserSync.reload();
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
